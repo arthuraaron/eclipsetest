0628ef774a3d3b49222b035c15b34a8e2d1dd847
aaron.d.arthur <aaron.d.arthur@CLC-LAB209-10.TCSD.tooelesd.org>
2018-09-04 13:18:59 -0600

    first commit

---
+++ src/aaron/arthurHelloWorld/HelloWorld.java
@@ -0,0 +1,19 @@
+/**
+ * hello world program print hello world in console
+ */
+package aaron.arthurHelloWorld;
+
+/**
+ * @author aaron.d.arthur
+ * 9/18
+ * Hello World Program
+ *
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+
+	}
+
+}
